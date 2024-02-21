166cdbef9f9fdb22bd6ce73f4cd681ccb06cecdf
Samuel Castro <dreift133@gmail.com>
2024-02-21 22:28:42 +0100

    cambio de bbdd

--- QuizGame.sql
+++ QuizGame.sql
@@ -1,5 +1,6 @@
-create database quizgame;
-use quizgame;
+CREATE DATABASE IF NOT EXISTS quizgame;
+USE quizgame;
+
 CREATE TABLE Usuario (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255),
@@ -11,6 +12,7 @@ CREATE TABLE Tematica (
     id_tematica INT AUTO_INCREMENT PRIMARY KEY,
     nombre_tematica VARCHAR(255)
 );
+
 CREATE TABLE Pregunta (
     id_pregunta INT AUTO_INCREMENT PRIMARY KEY,
     texto_pregunta TEXT,
@@ -24,20 +26,14 @@ CREATE TABLE Pregunta (
 
 CREATE TABLE Comentario (
     id_comentario INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT,
+    Nombre varchar(300),
     comentario TEXT,
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
 );
-
-
-
-
-
 
 INSERT INTO Tematica (nombre_tematica) VALUES ('Biología');
 
--- Preguntas de Biología
 INSERT INTO Pregunta (texto_pregunta, respuesta_correcta, opcion_incorrecta1, opcion_incorrecta2, opcion_incorrecta3, id_tematica)
 VALUES 
     ('¿Cuál es el gas esencial para la respiración de los seres vivos?', 'b) Oxígeno (O2)', 'a) Dióxido de carbono (CO2)', 'c) Nitrógeno (N2)', 'd) Hidrógeno (H2)', 1),
@@ -45,9 +41,7 @@ VALUES
     ('¿Cuál es el órgano principal del sistema digestivo?', 'b) Estómago', 'a) Hígado', 'c) Riñones', 'd) Intestino delgado', 1),
     ('¿Cuál es la función principal de los pulmones en el sistema respiratorio?', 'c) Intercambio de gases', 'a) Producción de oxígeno', 'b) Filtrar el aire', 'd) Almacenar dióxido de carbono', 1);
 
--- Geometría
 INSERT INTO Tematica (nombre_tematica) VALUES ('Geometría');
-
 
 INSERT INTO Pregunta (texto_pregunta, respuesta_correcta, opcion_incorrecta1, opcion_incorrecta2, opcion_incorrecta3, id_tematica)
 VALUES
@@ -58,13 +52,11 @@ VALUES
 
 INSERT INTO Tematica (nombre_tematica) VALUES ('Física');
 
--- Preguntas de Física
 INSERT INTO Pregunta (texto_pregunta, respuesta_correcta, opcion_incorrecta1, opcion_incorrecta2, opcion_incorrecta3, id_tematica)
 VALUES
     ('¿Cuál de las siguientes unidades se utiliza para medir la intensidad de corriente eléctrica?', 'c) Amperio (A)', 'a) Voltio (V)', 'b) Ohmio (Ω)', 'd) Culombio (C)', 3),
     ('¿En qué unidad se mide la fuerza en el sistema internacional?', 'a) Newton (N)', 'b) Julios (J)', 'c) Vatios (W)', 'd) Pascal (Pa)', 3),
     ('¿Cuál es la fórmula para calcular la fuerza de la gravedad?', 'd) F = Gm1m2/r2', 'a) n=r*q', 'b) n=r*r', 'c) n=q*q', 3),
     ('¿Cuál es la fórmula para calcular la energía cinética de un objeto en movimiento?', 'b) E = 1/2mv^2', 'a) E = mgh', 'c) E = m/c^2', 'd) E = Fd', 3);
-    
-    
+
     
